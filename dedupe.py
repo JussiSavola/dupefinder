8848ecc422feb0096b60c41e9e7ea57101d1c03f
JussiSavola <JussiSavola@users.noreply.github.com>
2017-04-19 21:02:27 +0300

    Add files via upload
    Initial versions.

---
+++ dedupe.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# dupe finder - Jussi Savola 2013, 2017
+# This program creates a sqlite3 db and enters information on files into the database
+# after this, user can use the db to find out duplicate files :)
+#
+# Usage: python dupe.py [optional directory]
+#
+# Disclaimer: If this breaks your files or your system, you can keep all the parts
+
+import sqlite3
+import os
+import sys
+import fnmatch
+from os.path import join, getsize, exists
+import hashlib
+from stat import *
+from sys import argv
+import socket
+
+
+def create_db_table(c):
+    c.execute('''CREATE TABLE files
+        (hostname text, fullname text, size text, md5sum text, filetime text, nukeme text, mastercopy text)''')
+
+
+def output_dupes(c):
+    c.execute("select md5sum, size, fullname  from files where (md5sum) in "
+              "(select md5sum from files group by md5sum having count(*) > 1) order by md5sum")
+
+    rows = c.fetchall()
+    print rows
+    for row in rows:
+        print row
+
+def is_regular(fullname):
+    try:
+        mode = os.stat(fullname).st_mode
+        if S_ISREG(mode) and not os.path.islink(fullname):  # Only regular files considered
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def skip_this(fullname):
+    if ".DS_Store" in fullname or '.PKG' in fullname or  '.media' in fullname:
+        return True
+    else:
+        return False
+
+def main(argv):
+    mysystemname = socket.gethostname()
+    print ("my system name", mysystemname)
+    dbfile = mysystemname + ".db"
+    print ("db file name: ", dbfile)
+    print ("in main")
+    print ("fs encoding is ", sys.getfilesystemencoding())
+    print
+
+    minsize = 40000
+    i = 0
+    file_start_size = 32000
+    file_end_size = 32000
+
+    if not exists(dbfile):
+        conn = sqlite3.connect(dbfile)
+        print ("conn", conn)
+        c = conn.cursor()
+        print ("c", c)
+        print ("db file does not exist. creating")
+        create_db_table(c)
+        conn.commit()
+        conn.close()
+
+    conn = sqlite3.connect(dbfile)
+    c = conn.cursor()
+
+    if len(argv) > 1:
+        start_point = argv[1]
+    else:
+        start_point = '.'
+
+    for root, dirs, files in os.walk(start_point):
+        for nativename in files:
+            i += 1
+            if i > 100:
+                i = 0
+                conn.commit()
+
+            # print ("name: ", nativename)
+            try:
+                fullname = join(root, nativename)
+                if not skip_this(fullname):
+                    try:
+                        statinfo = os.stat(fullname)
+                        filesize = statinfo.st_size
+                    except:
+                        filesize = 0
+                    # print ("File exists.", fullname)
+                    if filesize > minsize and is_regular(fullname):
+                        current_file = open(fullname, 'rb')
+                        try:
+                            file_beginning = current_file.read(file_start_size)
+                            current_file.seek(-file_end_size, 2)  # last bytes of the file
+                            file_end = current_file.read(file_end_size)
+                            file_chunk = file_beginning + file_end
+                            myhash = hashlib.md5(file_chunk).hexdigest()
+                        # print ("myhash is ", myhash)
+                        except:
+                            print ("Unable to read the file")
+                            break
+                        current_file.close()
+
+                        print "full filename, size:", fullname, filesize
+                        utf8name = unicode(fullname, 'latin-1')
+
+                        c.execute("INSERT INTO files VALUES (?,?,?,?,?,?,?)",
+                            (mysystemname, utf8name, filesize, myhash, '3','0','0'))
+                    else:
+                        try:
+                            print (str(fullname) + " file size too small (" + str(filesize) + "), or some other"
+                                   " other oddity. maybe a link.")
+                        except:
+                            print ("Something too funny with filename, skipping")
+                            print
+                else:
+                    print ("skipping: ", fullname)
+
+            except IOError as e:
+                print ("kuukkeliskuu")
+
+    output_dupes(c)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    main(argv)
---
+++ dedupeo.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python2.7
+
+# -*- coding: utf-8 -*-
+# dupe finder - Jussi Savola 2013, 2017
+# This program creates a sqlite3 db and enters information on files into the database
+# after this, user can use the db to find out duplicate files :)
+#
+# Usage: python dupe.py [optional directory]
+#
+# Disclaimer: If this breaks your files or your system, you can keep all the parts
+
+import sqlite3
+import os
+import sys
+from os.path import join, getsize, islink, isdir, basename
+import hashlib
+from sys import argv
+import socket
+
+SKIP_FILES = ['.DS_Store', '.PKG', '.media']
+DEBUG = 1
+
+
+def warn(msg):
+    sys.stderr.write(msg + "\n")
+
+
+def debug(msg):
+    if DEBUG > 0:
+        warn(msg)
+
+
+class MemStore:
+    def __init__(self, name):
+        self.name = name
+        self.store = []
+
+    def add(self, filename, filesize, digest, ftime):
+        self.store.append((self.name, filename, filesize, digest, ftime))
+
+    def output_dupes(self):
+        print("Printing all dupes: ")
+        for (sysname, filename, size, digest, ftime) in self.store:
+            print (digest, size, filename)
+
+    def commit(self):
+        pass
+
+    def finish(self):
+        pass
+
+
+class DbStore:
+    def __init__(self, name):
+        self.name = name
+        self.conn = sqlite3.connect(name)
+
+        self.__ensure_table_present()
+
+        self.cursor = self.conn.cursor()
+        self.buf_count = 0
+        self.batch_size = 100
+
+    def add(self, filename, filesize, digest, ftime):
+        sql = "INSERT INTO files VALUES (?,?,?,?,?,?,?)"
+        self.cursor.execute(sql, (self.name, filename, filesize, digest, ftime,'0','0'))
+
+        self.buf_count += 1
+        if self.buf_count >= self.batch_size:
+            self.commit()
+            self.buf_count = 0
+
+    def commit(self):
+        self.conn.commit()
+
+    def finish(self):
+        self.conn.close()
+
+    def output_dupes(self):
+        self.cursor.execute("select md5sum, size, fullname from files where (md5sum) in "
+                            "(select md5sum from files group by md5sum having count(*) > 1) order by md5sum")
+
+        rows = self.cursor.fetchall()
+        print
+        print("Printing all dupes: ")
+        for row in rows:
+            print row
+
+    def __ensure_table_present(self):
+        self.conn.cursor().execute('''CREATE TABLE IF NOT EXISTS files
+            (hostname text, fullname text, size text, md5sum text, filetime text, nukeme text, mastercopy text)''')
+        self.conn.commit()
+
+
+class DupChecker:
+    def __init__(self, store, minsize=40000, blocksize=16000, skip_files=[]):
+        self.store = store
+        self.minsize = minsize
+        self.blocksize = blocksize
+        self.skip_files = SKIP_FILES + skip_files
+
+    @staticmethod
+    def is_regular(fpath):
+        return os.path.isfile(fpath) and not os.path.islink(fpath)
+
+    @staticmethod
+    def trim_str(str, maxlen):
+        if len(str) > maxlen:
+            chunk = maxlen/2 - 2
+            return str[0:chunk] + '...' + str[-chunk:]
+        else:
+            return str
+
+    def store_checksum(self, fullname, filesize):
+        current_file = open(fullname, 'rb')
+        try:
+            file_beginning = current_file.read(self.blocksize)
+            current_file.seek(-self.blocksize, 2)  # last bytes of the file
+            file_end = current_file.read(self.blocksize)
+            file_chunk = file_beginning + file_end
+            digest = hashlib.md5(file_chunk).hexdigest()
+        except StandardError, err:
+            warn("Unable to read file ", fullname, err)
+            return
+        finally:
+            current_file.close()
+
+        self.store.add(unicode(fullname, 'latin-1'), filesize, digest, '3')
+        return digest
+
+    def skippable(self, filename):
+        basename(filename) in self.skip_files
+
+    def run(self, args):
+        print("fs encoding is ", sys.getfilesystemencoding())
+        print
+
+        if len(args) > 0:
+            start_point = args[0]
+        else:
+            start_point = '.'
+
+        debug("using {} as starting point".format(start_point))
+        print
+        fmt = "{:<60} {:>10} {:>10}"
+
+        debug(fmt.format("Full path", "Size", "Digest"))
+
+        for root, dirs, files in os.walk(start_point):
+            for nativename in files:
+
+                fullname = join(root, nativename)
+                if isdir(fullname) or islink(fullname) or self.skippable(fullname):
+                    continue
+
+                try:
+                    filesize = getsize(fullname)
+
+                    if not self.is_regular(fullname):
+                        debug("{}: is not a regular file, skipping".format(fullname))
+                    elif filesize < self.minsize:
+                        tmpl = "{}: file size too small ({}), not entering db"
+                        debug(tmpl.format(fullname, filesize))
+                    else:
+                        digest = self.store_checksum(fullname, filesize)
+
+                        debug(fmt.format(self.trim_str(fullname, 60), filesize, digest))
+
+                except IOError as e:
+                    print("kuukkeliskuu: " + e)
+
+        self.store.commit()
+        self.store.output_dupes()
+        self.store.finish()
+
+
+if __name__ == "__main__":
+    systemname = socket.gethostname()
+
+    print("my system name", systemname)
+    store_name = systemname + ".db"
+    print("db file name: ", store_name)
+
+    store = DbStore(store_name)
+    #  store = MemStore(store_name)
+
+    dc = DupChecker(store)
+    dc.run(argv[1:])
